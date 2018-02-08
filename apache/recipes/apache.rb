922f8a7f23df8769d5fd3d91ab757bdded920dbd
dineshvcloud <dinesh.vcloud@gmail.com>
2018-02-08 13:32:08 -0500

    start-index

--- apache/recipes/apache.rb
+++ apache/recipes/apache.rb
@@ -19,8 +19,35 @@ $ ps -ef | grep httpd
 
 file '/var/www/html/index.html' do
   content '<html>
-  <body>
-    <h1>hello world</h1>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Sample Deployment</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: #0000ff;
+      font-family: Arial, sans-serif;
+      font-size: 16px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {
+      font-size: 200%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+  </style>
+</head>
+<body>
+  <div align="center">
+    <h1>Congratulations!</h1>
+    <h2>Site is up and running.</h2>
+  </div>
+</body>
 </html>'
 end
